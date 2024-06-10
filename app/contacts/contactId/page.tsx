7cc0d63db42c2cf17d1b9fd59e6f62f1c0c441ae
pausm99 <pausm99@gmail.com>
2024-06-10 09:54:23 +0200

    initial agenda structure

---
+++ app/contacts/contactId/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contact() {
+  return (
+    <main>
+        <h1>Contact info</h1>
+    </main>
+  )
+}
---
+++ app/contacts/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contacts() {
+  return (
+    <main>
+        <h1>Contacts</h1>
+    </main>
+  )
+}
