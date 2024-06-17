3beef925429087667af0ab4fde3f86d03cc3dc59
pausm99 <pausm99@gmail.com>
2024-06-17 18:19:06 +0200

    updated not-found component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
 
 export default function NotFound() {
@@ -11,6 +13,7 @@ export default function NotFound() {
     >
       <h1 className="text-7xl text-stone-500">404</h1>
       <p>Page not found.</p>
+      <Link href="/agendas" className="mt-6"><Button>Return to dashboard</Button></Link>
     </main>
   );
 }
